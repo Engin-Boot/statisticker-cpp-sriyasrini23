dffe7ab20d31f99236c62236875ba3e1149375a4
sriyasrini23 <60133969+sriyasrini23@users.noreply.github.com>
2020-09-05 15:31:45 +0400

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,51 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Stats::Stats(): average(NAN), max(NAN), min(NAN) { }
+
+Stats Statistics::ComputeStatistics(const std::vector<float>& numbers) {
+    Stats result;
+
+    if (numbers.empty())
+        return result;
+    else
+    {
+        result.average = Statistics::Average(numbers);
+        result.max = Statistics::Max(numbers);
+        result.min = Statistics::Min(numbers);
+    }
+    return result;
+}
+
+float Statistics::Average(const std::vector<float>& numbers) {
+    
+    float sum = 0;
+    
+    for (int i = 0; i < numbers.size(); i++)
+    { sum += numbers[i]; }
+
+    float average = sum / numbers.size();
+
+    return average;
+}
+
+float Statistics::Max(const std::vector<float>& numbers)
+{
+    float max = 0;
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        if (numbers[i] > max)
+            max = numbers[i];
+    }
+    return max;
+}
+
+float Statistics::Min(const std::vector<float>& numbers)
+{
+    float min = 10000000;
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        if (numbers[i] < min)
+            min = numbers[i];
+    }
+    return min;
 }
